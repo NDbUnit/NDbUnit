e5e7d40dc8b654df828ca950d7da12cd394c286a
Steve Bohlen <sbohlen@gmail.com>
2009-03-17 00:03:58 +0000

    delete extraneous DB creation script and fold current DB schema definition into the correct create script in the /Scripts/ folder

--- NDbUnit.Test/Scripts/sqlserver-testdb-create.sql
+++ NDbUnit.Test/Scripts/sqlserver-testdb-create.sql
@@ -9,24 +9,77 @@ GO
 USE [testdb]
 GO
 
-CREATE TABLE [dbo].[Role] (
-	[ID] [int] IDENTITY NOT NULL PRIMARY KEY,
-	[Name] [varchar] (50) NOT NULL,
-	[Description] [varchar] (50)
-)
+/****** Object:  Table [dbo].[User]    Script Date: 02/22/2009 16:01:41 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[User](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [varchar](50) NOT NULL,
+	[LastName] [varchar](50) NOT NULL,
+	[Age] [int] NULL,
+	[SupervisorID] [int] NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[Role]    Script Date: 02/22/2009 16:01:39 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Role](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[Description] [varchar](50) NULL,
+ CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[UserRole]    Script Date: 02/22/2009 16:01:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserRole](
+	[UserID] [int] NOT NULL,
+	[RoleID] [int] NOT NULL,
+ CONSTRAINT [PK_UserRole] PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC,
+	[RoleID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  ForeignKey [FK_User_Supervisor]    Script Date: 02/22/2009 16:01:41 ******/
+ALTER TABLE [dbo].[User]  WITH CHECK ADD  CONSTRAINT [FK_User_Supervisor] FOREIGN KEY([SupervisorID])
+REFERENCES [dbo].[User] ([ID])
+GO
+ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_Supervisor]
+GO
+/****** Object:  ForeignKey [FK_UserRole_Role]    Script Date: 02/22/2009 16:01:42 ******/
+ALTER TABLE [dbo].[UserRole]  WITH CHECK ADD  CONSTRAINT [FK_UserRole_Role] FOREIGN KEY([RoleID])
+REFERENCES [dbo].[Role] ([ID])
+GO
+ALTER TABLE [dbo].[UserRole] CHECK CONSTRAINT [FK_UserRole_Role]
+GO
+/****** Object:  ForeignKey [FK_UserRole_User]    Script Date: 02/22/2009 16:01:42 ******/
+ALTER TABLE [dbo].[UserRole]  WITH CHECK ADD  CONSTRAINT [FK_UserRole_User] FOREIGN KEY([UserID])
+REFERENCES [dbo].[User] ([ID])
+GO
+ALTER TABLE [dbo].[UserRole] CHECK CONSTRAINT [FK_UserRole_User]
 GO
 
-CREATE TABLE [dbo].[User] (
-	[ID] [int] IDENTITY NOT NULL PRIMARY KEY,
-	[FirstName] [varchar] (50) NOT NULL,
-	[LastName] [varchar] (50) NOT NULL,
-	[Age] [smallint]
-)
-GO
-
-CREATE TABLE [dbo].[UserRole] (
-	[UserID] [int] NOT NULL REFERENCES [dbo].[User]([ID]),
-	[RoleID] [int] NOT NULL REFERENCES [dbo].[Role]([ID]),
-	PRIMARY KEY([UserID], [RoleID])
-)
-GO
