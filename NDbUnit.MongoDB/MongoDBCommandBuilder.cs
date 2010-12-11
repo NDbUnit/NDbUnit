78653de5df4851df2d6bcd96255ebdc1edb3bd9d
John Petersen <johnvpetersen@gmail.com>
2010-12-11 03:31:59 +0000

    Added initial project for MongoDB. Added MongoDB C# drivers to /lib folder.

---
+++ NDbUnit.MongoDB/MongoDBCommandBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+
+namespace NDbUnit.Core.MongoDB
+{
+    public class MongoDBCommandBuilder : DbCommandBuilder
+    {
+
+        public MongoDBCommandBuilder(IDbConnection connection)
+            : base(connection)
+        {
+        }
+
+        public MongoDBCommandBuilder(string connectionString)
+            : base(connectionString)
+        {
+        }
+
+
+        protected override IDbCommand CreateDbCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IDataParameter CreateNewSqlParameter(int index, DataRow dataRow)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IDbConnection GetConnection(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NDbUnit.MongoDB/MongoDBUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace NDbUnit.Core.MongoDB
+{
+    public class MongoDBUnitTest : NDbUnitTest
+    {
+        public MongoDBUnitTest(string connectionString) : base(connectionString)
+        {
+        }
+
+        public MongoDBUnitTest(IDbConnection connection) : base(connection)
+        {
+        }
+
+        protected override IDbDataAdapter CreateDataAdapter(IDbCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IDbCommandBuilder CreateDbCommandBuilder(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IDbCommandBuilder CreateDbCommandBuilder(IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IDbOperation CreateDbOperation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
