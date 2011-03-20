1a6e47058f417a22c1989f720a632882b4c03809
Steve Bohlen <sbohlen@gmail.com>
2011-03-20 19:21:33 +0000

    correct errant casing in tablenames so that collation in the DB != failing tests

--- NDbUnit.Test/Scripts/mysql-testdb-create.sql
+++ NDbUnit.Test/Scripts/mysql-testdb-create.sql
@@ -3,34 +3,34 @@ CREATE DATABASE `testdb`;
 
 SET FOREIGN_KEY_CHECKS = 0;
 
-DROP TABLE IF EXISTS `testdb`.`role`;
-CREATE TABLE  `testdb`.`role` (
+DROP TABLE IF EXISTS `testdb`.`Role`;
+CREATE TABLE  `testdb`.`Role` (
   `ID` bigint(20) NOT NULL auto_increment,
   `Name` varchar(45) default NULL,
   `Description` varchar(45) default NULL,
   PRIMARY KEY  (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `testdb`.`user`;
-CREATE TABLE  `testdb`.`user` (
+DROP TABLE IF EXISTS `testdb`.`User`;
+CREATE TABLE  `testdb`.`User` (
   `ID` bigint(20) NOT NULL auto_increment,
   `FirstName` varchar(45) default NULL,
   `LastName` varchar(45) default NULL,
   `Age` bigint(20) unsigned default NULL,
   `SupervisorID` bigint(20) default NULL,
   PRIMARY KEY  (`ID`),
-  KEY `FK_user_supervisor` (`SupervisorID`),
-  CONSTRAINT `FK_user_supervisor` FOREIGN KEY (`SupervisorID`) REFERENCES `user` (`ID`) ON DELETE SET NULL ON UPDATE CASCADE
+  KEY `FK_User_supervisor` (`SupervisorID`),
+  CONSTRAINT `FK_User_supervisor` FOREIGN KEY (`SupervisorID`) REFERENCES `User` (`ID`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `testdb`.`userrole`;
-CREATE TABLE  `testdb`.`userrole` (
+DROP TABLE IF EXISTS `testdb`.`UserRole`;
+CREATE TABLE  `testdb`.`UserRole` (
   `UserID` bigint(20) NOT NULL,
   `RoleID` bigint(20) NOT NULL,
   PRIMARY KEY  (`UserID`,`RoleID`),
-  KEY `FK_userrole_role` (`RoleID`),
-  CONSTRAINT `FK_userrole_role` FOREIGN KEY (`RoleID`) REFERENCES `role` (`ID`),
-  CONSTRAINT `FK_userrole_user` FOREIGN KEY (`UserID`) REFERENCES `user` (`ID`)
+  KEY `FK_UserRole_Role` (`RoleID`),
+  CONSTRAINT `FK_UserRole_Role` FOREIGN KEY (`RoleID`) REFERENCES `Role` (`ID`),
+  CONSTRAINT `FK_UserRole_User` FOREIGN KEY (`UserID`) REFERENCES `User` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 SET FOREIGN_KEY_CHECKS = 1;
