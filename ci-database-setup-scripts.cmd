2b6f71fecc2b73dd3085628eda1316b40c48760a
Steve Bohlen <sbohlen@gmail.com>
2014-12-31 08:33:05 -0500

    removing command-line tools for databases from repo; revert to assuming on CI server path; move setting of PGPASSWORD env var to the script vs. the CI server setup so more seettings localized to the script

--- ci-database-setup-scripts.cmd
+++ ci-database-setup-scripts.cmd
@@ -4,12 +4,16 @@ rem SQL Server setup *****
 sqlcmd -S ".\SQL2008R2SP2" -U sa -P Password12! -i "%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\sqlserver-testdb-create.sql"
 
 rem MySQL setup *****
-"%APPVEYOR_BUILD_FOLDER%\tools\MySql\bin\mysql" --user=root --password=Password12! < "%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\mysql-testdb-create.sql"
+mysql --user=root --password=Password12! < "%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\mysql-testdb-create.sql"
 
 rem PostgreSQL setup *****
+rem NOTE: postgres doesn't support password as args to psql so ensure that PGPASSWORD env var is set...
+set PGPASSWORD=Password12!
+
 rem NOTE: postgres won't support a CREATE DATABASE call inside a larger script, so CREATE has to be its own invocation...
-"%APPVEYOR_BUILD_FOLDER%\tools\Postgres\bin\psql" --username=postgres --no-password --command="CREATE DATABASE testdb;"
-"%APPVEYOR_BUILD_FOLDER%\tools\Postgres\bin\psql" --username=postgres --no-password --dbname=testdb --file="%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\postgres-testdb-create.sql"
+psql --username=postgres --no-password --command="CREATE DATABASE testdb;"
+
+psql --username=postgres --no-password --dbname=testdb --file="%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\postgres-testdb-create.sql"
 
 rem ORACLE XE setup *****
 rem *** DISABLED UNTIL APPVEYOR SUPPORTS ORA XE ON CI SERVERS ***
