fffe295dc234a212d155d8981e959a2d04f63bae
Steve Bohlen <sbohlen@gmail.com>
2014-12-30 22:12:45 -0500

    update CI db scripts to copy the pgpass.conf file to support postgres access via command-line

--- ci-database-setup-scripts.cmd
+++ ci-database-setup-scripts.cmd
@@ -1,3 +1,5 @@
+@echo on
+
 rem SQL Server setup *****
 sqlcmd -S ".\SQL2008R2SP2" -U sa -P Password12! -i "%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\sqlserver-testdb-create.sql"
 
@@ -5,9 +7,18 @@ rem MySQL setup *****
 "%APPVEYOR_BUILD_FOLDER%\tools\MySql\bin\mysql" --user=root --password=Password12! < "%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\mysql-testdb-create.sql"
 
 rem PostgreSQL setup *****
+rem Since postgres doesn't support passing passwords via the command-line, have to copy the pgpass.conf file into the user's folder so it can be found when invoking psql
+
+rem first create the folder so the copy will succeeed...
+mkdir %APPDATA%\postgresql
+
+rem ...then do the copy...
+copy "%APPVEYOR_BUILD_FOLDER%\tools\Postgres\pgpass.conf" %APPDATA%\postgresql\pgpass.conf
+
 rem NOTE: postgres won't support a CREATE DATABASE call inside a larger script, so CREATE has to be its own invocation...
-rem "%APPVEYOR_BUILD_FOLDER%\tools\Postgres\bin\psql" --username=postgres --no-password --command="CREATE DATABASE testdb;"
-rem "%APPVEYOR_BUILD_FOLDER%\tools\Postgres\bin\psql" --username=postgres --no-password --dbname=testdb --file="%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\postgres-testdb-create.sql"
+"%APPVEYOR_BUILD_FOLDER%\tools\Postgres\bin\psql" --username=postgres --no-password --command="CREATE DATABASE testdb;"
+
+"%APPVEYOR_BUILD_FOLDER%\tools\Postgres\bin\psql" --username=postgres --no-password --dbname=testdb --file="%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\postgres-testdb-create.sql"
 
 rem ORACLE XE setup *****
 rem *** DISABLED UNTIL APPVEYOR SUPPORTS ORA XE ON CI SERVERS ***
