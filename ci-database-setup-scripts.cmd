ce4ff685c5c1696d0626473b23816d6e6ce93814
Steve Bohlen <sbohlen@gmail.com>
2014-12-30 21:23:37 -0500

    adding mysql binaries for MySql since not installed on CI server

--- ci-database-setup-scripts.cmd
+++ ci-database-setup-scripts.cmd
@@ -2,12 +2,12 @@ rem SQL Server setup *****
 sqlcmd -S ".\SQL2008R2SP2" -U sa -P Password12! -i "%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\sqlserver-testdb-create.sql"
 
 rem MySQL setup *****
-mysql --user=root --password=Password12! < "%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\mysql-testdb-create.sql"
+"%APPVEYOR_BUILD_FOLDER%\NDbUnit\tools\MySql\bin\mysql" --user=root --password=Password12! < "%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\mysql-testdb-create.sql"
 
 rem PostgreSQL setup *****
 rem NOTE: postgres won't support a CREATE DATABASE call inside a larger script, so CREATE has to be its own invocation...
-"%APPVEYOR_BUILD_FOLDER%\NDbUnit\tools\postgres\bin\psql --username=postgres --no-password --command="CREATE DATABASE testdb;"
-"%APPVEYOR_BUILD_FOLDER%\NDbUnit\tools\postgres\bin\psql --username=postgres --no-password --dbname=testdb --file="%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\postgres-testdb-create.sql"
+"%APPVEYOR_BUILD_FOLDER%\NDbUnit\tools\Postgres\bin\psql" --username=postgres --no-password --command="CREATE DATABASE testdb;"
+"%APPVEYOR_BUILD_FOLDER%\NDbUnit\tools\Postgres\bin\psql" --username=postgres --no-password --dbname=testdb --file="%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\postgres-testdb-create.sql"
 
 rem ORACLE XE setup *****
 rem *** DISABLED UNTIL APPVEYOR SUPPORTS ORA XE ON CI SERVERS ***
