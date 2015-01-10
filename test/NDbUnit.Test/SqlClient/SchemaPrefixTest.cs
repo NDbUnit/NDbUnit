2672163fcd8ef9f16c5cc27b08523e127c9f9ee9
Steve Bohlen <sbohlen@gmail.com>
2015-01-10 08:48:37 -0500

    refactor tests to avoid multiply-nested fixtures (not discoverable by test-runner so tests not actually being invoked properly)

--- test/NDbUnit.Test/SqlClient/SchemaPrefixTest.cs
+++ test/NDbUnit.Test/SqlClient/SchemaPrefixTest.cs
@@ -6,28 +6,22 @@ namespace NDbUnit.Test.SqlClient
     [TestFixture]
     public class SchemaPrefixTest
     {
-
-        [TestFixture]
-        public class When_Schema_XSD_File_Contains_DBSchema_Prefixes
+        [Test]
+        public void Can_Perform_CleanInsertUpdate_Operation_Without_Exception_When_Schema_Has_Prefix()
         {
-            [Test]
-            public void Can_Perform_CleanInsertUpdate_Operation_Without_Exception()
+            try
             {
-                try
-                {
-                    var db = new NDbUnit.Core.SqlClient.SqlDbUnitTest(DbConnection.SqlConnectionString);
-                    db.ReadXmlSchema(XmlTestFiles.SqlServer.XmlSchemaFileForSchemaPrefixTests);
-                    db.ReadXml(XmlTestFiles.SqlServer.XmlFileForSchemaPrefixTests);
+                var db = new NDbUnit.Core.SqlClient.SqlDbUnitTest(DbConnection.SqlConnectionString);
+                db.ReadXmlSchema(XmlTestFiles.SqlServer.XmlSchemaFileForSchemaPrefixTests);
+                db.ReadXml(XmlTestFiles.SqlServer.XmlFileForSchemaPrefixTests);
 
-                    db.PerformDbOperation(NDbUnit.Core.DbOperationFlag.CleanInsertIdentity);
-                }
-                catch (Exception)
-                {
-                    Assert.Fail("Operation not successful when using tables with Schema Prefixes.");
-                }
+                db.PerformDbOperation(NDbUnit.Core.DbOperationFlag.CleanInsertIdentity);
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Operation not successful when using tables with Schema Prefixes.");
+            }
 
-            }    
         }
-        
     }
 }
