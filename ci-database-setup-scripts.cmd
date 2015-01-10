7f1ad22291cd3f2d181782fa83e7bb57e8ba8d7c
Steve Bohlen <sbohlen@gmail.com>
2015-01-10 09:17:19 -0500

    fixing to reflect new /test/ parent folder hierarchy

--- ci-database-setup-scripts.cmd
+++ ci-database-setup-scripts.cmd
@@ -1,10 +1,10 @@
 @echo on
 
 rem SQL Server setup *****
-sqlcmd -S ".\SQL2008R2SP2" -U sa -P Password12! -i "%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\sqlserver-testdb-create.sql"
+sqlcmd -S ".\SQL2008R2SP2" -U sa -P Password12! -i "%APPVEYOR_BUILD_FOLDER%\test\NDbUnit.Test\Scripts\sqlserver-testdb-create.sql"
 
 rem MySQL setup *****
-mysql --user=root --password=Password12! < "%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\mysql-testdb-create.sql"
+mysql --user=root --password=Password12! < "%APPVEYOR_BUILD_FOLDER%\test\NDbUnit.Test\Scripts\mysql-testdb-create.sql"
 
 rem PostgreSQL setup *****
 rem NOTE: postgres doesn't support password as args to psql so ensure that PGPASSWORD env var is set...
@@ -13,9 +13,9 @@ set PGPASSWORD=Password12!
 rem NOTE: postgres won't support a CREATE DATABASE call inside a larger script, so CREATE has to be its own invocation...
 psql --username=postgres --no-password --command="CREATE DATABASE testdb;"
 
-psql --username=postgres --no-password --dbname=testdb --file="%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\postgres-testdb-create.sql"
+psql --username=postgres --no-password --dbname=testdb --file="%APPVEYOR_BUILD_FOLDER%\test\NDbUnit.Test\Scripts\postgres-testdb-create.sql"
 
 rem ORACLE XE setup *****
 rem *** DISABLED UNTIL APPVEYOR SUPPORTS ORA XE ON CI SERVERS ***
-rem %ORACLE_HOME%\bin\sqlplus xdba/xdba @"%APPVEYOR_BUILD_FOLDER%\NDbUnit.Test\Scripts\oracle-testdb-create.sql"
+rem %ORACLE_HOME%\bin\sqlplus xdba/xdba @"%APPVEYOR_BUILD_FOLDER%\test\NDbUnit.Test\Scripts\oracle-testdb-create.sql"
 
