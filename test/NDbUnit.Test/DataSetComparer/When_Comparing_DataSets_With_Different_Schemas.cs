1211b29af91b72296865e231d536e9c775b9b28a
Steve Bohlen <sbohlen@gmail.com>
2015-01-12 11:47:23 -0500

    continuing to flesh out tests and test datasets

--- test/NDbUnit.Test/DataSetComparer/When_Comparing_DataSets_With_Different_Schemas.cs
+++ test/NDbUnit.Test/DataSetComparer/When_Comparing_DataSets_With_Different_Schemas.cs
@@ -1,7 +1,32 @@
-﻿namespace NDbUnit.Test.DataSetComparer
+﻿using NUnit.Framework;
+
+namespace NDbUnit.Test.DataSetComparer
 {
+    [TestFixture]
     public class When_Comparing_DataSets_With_Different_Schemas
     {
-         
+        [Test]
+        public void CanReportNoMatchOnMissingFK()
+        {
+            
+        }
+
+        [Test]
+        public void CanReportNoMatchOnDifferntDataTypes()
+        {
+            
+        }
+
+        [Test]
+        public void CanReportNoMatchOnDifferentColumnNames()
+        {
+            
+        }
+
+        [Test]
+        public void CanReportNoMatchOnDifferentTables()
+        {
+            
+        }
     }
 }
