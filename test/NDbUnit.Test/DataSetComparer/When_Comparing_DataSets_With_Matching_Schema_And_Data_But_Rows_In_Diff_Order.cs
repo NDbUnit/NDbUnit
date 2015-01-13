029b6a65086a59697df59bb3ea6a761526ff5aff
Steve Bohlen <sbohlen@gmail.com>
2015-01-12 21:52:40 -0500

    adding test to cover case where schemas and data match but rows are in a different order

---
+++ test/NDbUnit.Test/DataSetComparer/When_Comparing_DataSets_With_Matching_Schema_And_Data_But_Rows_In_Diff_Order.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using NDbUnit.Core;
+using NUnit.Framework;
+
+namespace NDbUnit.Test.DataSetComparer
+{
+    public class When_Comparing_DataSets_With_Matching_Schema_And_Data_But_Rows_In_Diff_Order : DataSetComparerTestBase
+    {
+        public void CanReportNoMatch()
+        {
+            var firstDataSet = BuildDataSet(@"Xml\DataSetComparer\FirstDataSetToCompare.xsd", @"Xml\DataSetComparer\FirstDataToCompare.xml");
+            var secondDataSet = BuildDataSet(@"Xml\DataSetComparer\FirstDataSetToCompare.xsd", @"Xml\DataSetComparer\DifferingDataWithRowsInDiffOrderToCompare.xml");
+
+            Assert.That(firstDataSet.HasTheSameSchemaAs(secondDataSet));
+            Assert.That(firstDataSet.HasTheSameDataAs(secondDataSet), Is.False);
+        }
+    }
+}
