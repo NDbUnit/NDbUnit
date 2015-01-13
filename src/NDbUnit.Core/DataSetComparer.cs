72f49c4f01adad282cdfe2781be823dc69b903f4
Steve Bohlen <sbohlen@gmail.com>
2015-01-12 20:40:21 -0500

    schema comparison of DataTables introduced

--- src/NDbUnit.Core/DataSetComparer.cs
+++ src/NDbUnit.Core/DataSetComparer.cs
@@ -1,10 +1,14 @@
 ﻿using System.Data;
 using System.Linq;
+using KellermanSoftware.CompareNetObjects;
 
 namespace NDbUnit.Core
 {
     public static class DataSetComparer
     {
+
+        private static CompareLogic _comparer = new CompareLogic();
+
         public static bool HasSameDataAs(this DataSet left, DataSet right)
         {
             //if schemas don't match, no point in proceeding to test any data/content so just bail out early...
@@ -23,22 +27,49 @@ namespace NDbUnit.Core
             if (leftTables.Count != rightTables.Count)
                 return false;
 
+            //consider tables
             foreach (var table in leftTables.Cast<DataTable>())
             {
                 if (!rightTables.Contains(table.TableName))
                     return false;
 
-                if (!AreTheSame(table, rightTables[table.TableName]))
+                if (!AreTheSameSchema(table, rightTables[table.TableName]))
                     return false;
             }
 
 
-            return false;
+            return true;
         }
 
-        private static bool AreTheSame(DataTable left, DataTable right)
+        private static bool AreTheSameSchema(DataTable left, DataTable right)
         {
-            return false;
+            var config = new ComparisonConfig { IgnoreCollectionOrder = true, CompareChildren = false };
+            config.MembersToIgnore.Add("Columns");
+            config.MembersToIgnore.Add("Rows");
+
+            _comparer.Config = config;
+
+            var result = _comparer.Compare(left, right);
+
+            if (!result.AreEqual)
+                return false;
+
+            //if the count of columns fails to match, no point in proceeding
+            if (left.Columns.Count != right.Columns.Count)
+                return false;
+
+            foreach (var element in left.Columns)
+            {
+                var column = (DataColumn)element;
+
+                if (!right.Columns.Contains(column.ColumnName))
+                    return false;
+
+                if (!AreTheSameSchema(column, right.Columns[column.ColumnName]))
+                    return false;
+            }
+
+            return true;
         }
 
         private static bool AreTheSame(DataRow left, DataRow right)
@@ -46,9 +77,14 @@ namespace NDbUnit.Core
             return false;
         }
 
-        private static bool AreTheSame(DataColumn left, DataColumn right)
+        private static bool AreTheSameSchema(DataColumn left, DataColumn right)
         {
-            return false;
+            var config = new ComparisonConfig { IgnoreCollectionOrder = true, CompareChildren = false };
+            _comparer.Config = config;
+
+            var result = _comparer.Compare(left, right);
+
+            return result.AreEqual;
         }
     }
 }
